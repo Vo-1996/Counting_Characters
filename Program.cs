872490ef22c8fdbb9bf1ee2c003fcd3c96b096a0
Vo-1996 <73895980+Vo-1996@users.noreply.github.com>
2023-04-03 23:43:42 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Counting_Characters // refer to folder
+{
+    class Program
+    {
+        public static void Counting_Characters(string word)
+        {
+            string newName = word.ToUpper();
+            Dictionary<char, int> myString = new Dictionary<char, int>();
+
+            foreach(char c in newName)
+            {
+                myString.TryGetValue(c, out int value);
+                myString[c] = value +1;
+            }
+
+            foreach(var x in myString)
+            {
+                Console.WriteLine($"Numeber of character for : {x.Key} is {x.Value}");
+            }
+            Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            string word = " Hello World I'm Counting!";
+            Counting_Characters(word);
+        }
+    }
+}
